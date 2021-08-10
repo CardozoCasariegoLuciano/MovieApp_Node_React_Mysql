004ba279c11d48528c1f62f60a5c23dff7cb9a9a
luciano cardozo casariego <lucianoc.c117@gmail.com>
2021-08-10 13:45:35 -0300

    Listo el header

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../styles/components/header.scss";
+import {Link} from "react-router-dom"
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-content">
+        <Link to="/" className="header-logo">Peliculas</Link>
+        <div className="header-links_area">
+          <Link className="link" to="/add">Agregar pelicula</Link>
+          <Link className="link" to="/import">Impotar</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- frontend/src/layout/Mainlayout.js
+++ frontend/src/layout/Mainlayout.js
@@ -1,11 +1,11 @@
 import React from "react";
+import Header from "../components/Header"
 
 const Mainlayout = ({children}) => {
   return (
     <div>
-      <div>Hola header</div>
-      {children}
-      <div>Hola Footer</div>
+      <Header/>
+       {children}
     </div>
   );
 };
